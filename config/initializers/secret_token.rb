0597f0a6666a5a3db4d348c3e604bc3d25baf7aa
Steve Kenworthy <steveyken@gmail.com>
2015-03-18 12:33:08 +0800

    Update to Rails 4.2 version

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FatFreeCRM::Application.config.secret_token = '60deddc02b3c097271f10d895ba2d9ec27b76da760957e1bf01ce642a8567a21dae42c1edae5b9ad622608b3f9fcfc3e9231bcdc7376c9621aad7bcd20ed7dfc'
